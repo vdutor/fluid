bae8242046b8ca5424df3fae285e20656c71c087
Vincent Dutordoir <dutordoirv@gmail.com>
2022-07-12 22:32:22 +0100

    Disposal

--- fluid/signal.py
+++ fluid/signal.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
+from tkinter import CURRENT
 from typing import Callable, Generic, Iterable, Set, TypeVar
 
 # from .pyscript import console
@@ -29,21 +30,43 @@ class Computation(Generic[R]):
     children: Set[Computation] = field(default_factory=set)
     """List of `Computation`s owned by this computation"""
 
-    def reset(self) -> None:
-        _execute_functions(list(self.cleanups))
+    def __post_init__(self):
+        # add clean up method to list
+        self.cleanups.add(self._remove_computation_from_signal_subscription_list)
+
+    def _remove_computation_from_signal_subscription_list(self) -> None:
         for signal in self.sources:
             signal.subscribed_computations.remove(self)
         self.sources.clear()
 
+    def cleanup(self) -> None:
+        list(map(lambda func: func(), list(self.cleanups)))
+        list(map(lambda child: child.cleanup(), list(self.children)))
+        self.children.clear()
+
     def execute(self) -> R:
         global OWNER
-        OWNER = self
-        self.reset()
+        global CURRENT_COMPUTATION
+
+        prev_current_computation = CURRENT_COMPUTATION
+        prev_owner = OWNER
+
+        if self.owner is not None:
+            # TODO: remove the None
+            self.owner.children.add(self)
+
+        self.cleanup()
+        OWNER = CURRENT_COMPUTATION = self
+
         try:
             assert self.function is not None
             return self.function()
         finally:
-            OWNER = self.owner
+            OWNER = prev_owner
+            CURRENT_COMPUTATION = prev_current_computation
+        
+    def is_root(self):
+        return self.function is None
 
 
 class Signal(Generic[T]):
@@ -66,6 +89,8 @@ class Signal(Generic[T]):
 
 # Globals:
 OWNER: Computation | None = None
+CURRENT_COMPUTATION: Computation | None = None
+ROOT: Computation = Computation(None)
 
 
 def _execute_functions(functions: Iterable[VoidFunc]) -> None:
@@ -78,16 +103,19 @@ def createEffect(function: Callable[[], R]) -> R:
 
     if OWNER is None:
         # TODO: raise warning
+        # raise Exception("Effects can not be created when owner is None")
         pass
 
     computation = Computation(function, owner=owner)
+
     return computation.execute()
 
 
 def createRoot(function: Callable[[], R]) -> R:
-    pass
+    OWNER = ROOT
+    return createEffect(function)
 
 
-def cleanUp(function: Callable) -> None:
-    # TODO
-    pass
+def cleanUp(function: VoidFunc) -> None:
+    if CURRENT_COMPUTATION is None: raise Exception("cleanUp's can only be added to computations")
+    CURRENT_COMPUTATION.cleanups.add(function)
